b8421bca6bdc7f8e94db42fee1695d1705f0663e
ElRoccinante <alaechouiye@gmail.com>
2024-08-13 05:58:52 +0000

    db.py

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import Session
+
+from user import Base
+from user import User
+from typing import TypeVar
+
+
+fields = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """add_user to add a new user in the db"""
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """find_user_by function"""
+        for x in kwargs:
+            if not kwargs or x not in fields:
+                raise InvalidRequestError
+        try:
+            return self._session.query(User).filter_by(**kwargs).one()
+        except Exception:
+            raise NoResultFound
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """update_user method"""
+        user_update = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if key not in fields:
+                raise ValueError
+            setattr(user_update, key, value)
+        self._session.commit()
